9bd801e5fce007811e198f790617b971f9ca41ea
Eber J <dev.eberr@gmail.com>
2024-04-11 13:01:37 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        /*
+        8 tipos primitivos
+        int, double, float, char, byte, short, long, boolean
+         */
+        int idade = 20;
+        long idadeLong = 10L;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+        System.out.println("A idade é "+idade);
+    }
+}
