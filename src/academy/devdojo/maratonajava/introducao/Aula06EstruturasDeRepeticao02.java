68f3576155d722f2447d784cde68622f2ddccd74
Eber J <dev.eberr@gmail.com>
2024-04-16 15:13:05 -0300

    adicionar exercício de for

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        // Exercício for
+        // Imprima todos os números pares de 0 até 1000
+        for (int i = 0; i < 30; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
