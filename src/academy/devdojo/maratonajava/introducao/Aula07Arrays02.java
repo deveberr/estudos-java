7414006ecd3937bb6b07135fec1f27c5056864af
Eber J <dev.eberr@gmail.com>
2024-04-17 16:18:51 -0300

    adicionar aula de arrays multidimensionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // Arrays Multidimensionais
+        int[][] meusNumeros = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(meusNumeros[0][2]);
+
+        // Alterar o valor de um elemento
+//        meusNumeros[1][2] = 7;
+//        System.out.println(meusNumeros[1][2]);
+
+        // Loop for dentro de um array multidimensional
+        // Posso usar um for loop dentro de outro for loop
+        for (int i = 0; i < meusNumeros.length; i++) {
+            for (int j = 0; j < meusNumeros[i].length; j++) {
+                System.out.println(meusNumeros[i][j]);
+            }
+        }
+
+        // Ou posso usar for-each que geralmente é mais fácil de ler e escrever
+        for (int[] j : meusNumeros) {
+            for (int i : j) {
+                System.out.println("FOR-EACH "+i);
+            }
+        }
+    }
+}
